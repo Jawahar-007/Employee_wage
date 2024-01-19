0bcba5f1ddf87f2910773c232f5c59a431d08f40
Jawahar-007 <jawaharbhaskaran@gmail.com>
2024-01-19 15:44:05 +0530

    Added : Switch case

--- src/empwage.java
+++ src/empwage.java
@@ -1,31 +1,34 @@
 public class empwage {
-    public static void main(String[] args) {
-        // Constants
-        int IS_PART_TIME = 1;
-        int IS_FULL_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
 
+    public static void main(String[] args) {
         // Varibles
         int empHrs = 0;
         int empWage = 0;
 
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_PART_TIME) {
-            empHrs = 4;
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            System.out.println("Emp Wage:" + empWage);
-        } else if (empCheck == IS_FULL_TIME) {
-            empHrs = 8;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case IS_PART_TIME:
+                empHrs = 4;
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                System.out.println("Emp Wage:" + empWage);
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                System.out.println("Emp Wage:" + empWage);
+                break;
 
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            System.out.println("Emp Wage:" + empWage);
-
-        } else {
-            empHrs = 0;
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            System.out.println("Emp Wage:" + empWage);
-
+            default:
+                empHrs = 0;
         }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage:" + empWage);
     }
-    
+
+
 }
+//Emp Wage:160
+//Emp Wage:160
