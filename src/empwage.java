78ac2f99f78a3f9dde2e887d489173191cc3c512
Jawahar-007 <jawaharbhaskaran@gmail.com>
2024-01-19 15:55:42 +0530

    Added: WagesforMonth

--- src/empwage.java
+++ src/empwage.java
@@ -2,33 +2,46 @@ public class empwage {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
-
-    public static void main(String[] args) {
-        // Varibles
-        int empHrs = 0;
+    public static final int NUM_OF_WORKING_DAY = 5;
+    public static void main(String[] args) { int empHrs = 0;
         int empWage = 0;
+        int totalEmpWage = 0;
 
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        switch (empCheck) {
-            case IS_PART_TIME:
-                empHrs = 4;
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                System.out.println("Emp Wage:" + empWage);
-                break;
-            case IS_FULL_TIME:
-                empHrs = 8;
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                System.out.println("Emp Wage:" + empWage);
-                break;
+        for (int day = 0; day < NUM_OF_WORKING_DAY; day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    empWage = empHrs * EMP_RATE_PER_HOUR;
+                    totalEmpWage += empWage;
+                    System.out.println("Emp Wag: " + empWage);
 
-            default:
-                empHrs = 0;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    empWage = empHrs * EMP_RATE_PER_HOUR;
+                    totalEmpWage += empWage;
+                    System.out.println("Emp Wag: " + empWage);
+                    break;
+
+                default:
+                    empHrs = 0;
+                    empWage = empHrs * EMP_RATE_PER_HOUR;
+                    totalEmpWage += empWage;
+                    System.out.println("Emp Wag: " + empWage);
+
+            }
+
         }
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Emp Wage:" + empWage);
+        System.out.println("Total Emp Wage : " + totalEmpWage);
     }
-
 
 }
 //Emp Wage:160
 //Emp Wage:160
+// UC5 Emp Wag: 0
+//Emp Wag: 80
+//Emp Wag: 0
+//Emp Wag: 80
+//Emp Wag: 80
+//Total Emp Wage : 240
