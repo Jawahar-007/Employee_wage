49b2698cb7b7d9821f41b6cbd2abc540718a6eae
Jawahar-007 <jawaharbhaskaran@gmail.com>
2024-01-24 13:32:08 +0530

    Added: Class method to compute EMPWAGE Multiple company

--- src/empwage.java
+++ src/empwage.java
@@ -1,40 +1,37 @@
 public class empwage {
-        public static final int IS_PART_TIME = 1;
-        public static final int IS_FULL_TIME = 2;
-        public static final int EMP_RATE_PER_HOUR = 20;
-        public static final int NUM_OF_WORKING_DAYS = 2;
-        public static final int MAX_HRS_IN_MONTH = 10;
-
-        public static int compEmpWage() {
-            int empHrs = 0;
-            int totalEmpHrs = 0;
-            int totalWorkingDays = 0;
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-                totalWorkingDays++;
-                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-                switch (empCheck) {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                }
-                totalEmpHrs += empHrs;
-                System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " + empHrs);
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static int compEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+        int empHrs = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
+        while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
+            totalWorkingDays++;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
             }
-            totalEmpHrs = totalEmpHrs * EMP_RATE_PER_HOUR;
-            System.out.println("Total Emp Wage: " + totalEmpHrs);
-            return totalEmpHrs;
+            totalEmpHrs += empHrs;
+            System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " + empHrs);
         }
-        public static void main(String[] args)
-        {
-            compEmpWage();
-        }
-
+        int totalEmpWage = totalEmpHrs * empRatePerHour;
+        System.out.println("Total Emp Wage for Company " +company+ " is:" + totalEmpWage);
+        return totalEmpWage;
     }
+    public static void main(String[] args)
+    {
+        compEmpWage("Valeo",10, 3, 20);
+        compEmpWage("Zoho",34, 4,65);
+    }
+
+}
 //Emp Wage:160
 //Emp Wage:160
 // UC5 Emp Wag: 0
