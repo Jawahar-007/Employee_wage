5afee7ea5b33730f70e057b8aceeae9a6b833384
Jawahar-007 <jawaharbhaskaran@gmail.com>
2024-01-24 14:42:31 +0530

    Added: Compute Employee Wage and Total Save for EACH COmpany

--- src/empwage.java
+++ src/empwage.java
@@ -1,7 +1,22 @@
+import java.sql.SQLOutput;
+
 public class empwage {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
-    public static int compEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+
+    private final String company;
+    private final int empRatePerHour;
+    private final int numOfWorkingDays;
+    private final int maxHoursPerMonth;
+    private int totalEmpWage;
+
+    public empwage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth){
+        this.company = company;
+        this.empRatePerHour = empRatePerHour;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHoursPerMonth = maxHoursPerMonth;
+    }
+    public void comEmpWage() {
         int empHrs = 0;
         int totalEmpHrs = 0;
         int totalWorkingDays = 0;
@@ -21,28 +36,22 @@ public class empwage {
             totalEmpHrs += empHrs;
             System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " + empHrs);
         }
-        int totalEmpWage = totalEmpHrs * empRatePerHour;
-        System.out.println("Total Emp Wage for Company " +company+ " is:" + totalEmpWage);
-        return totalEmpWage;
+        totalEmpWage = totalEmpHrs * empRatePerHour;
     }
+
+    @Override
+    public String toString() {
+        return "empwage{" + "company='" + company + '\'' + ", totalEmpWage=" + totalEmpWage + '}';
+    }
+
     public static void main(String[] args)
     {
-        compEmpWage("Valeo",10, 3, 20);
-        compEmpWage("Zoho",34, 4,65);
+        empwage Valeo = new empwage("Valeo",20,2,10);
+        empwage Databricks = new empwage("Databricks",10,4,20);
+        Valeo.comEmpWage();
+        System.out.println(Valeo);
+        Valeo.comEmpWage();
+        System.out.println(Databricks);
     }
 
 }
-//Emp Wage:160
-//Emp Wage:160
-// UC5 Emp Wag: 0
-//Emp Wag: 80
-//Emp Wag: 0
-//Emp Wag: 80
-//Emp Wag: 80
-//Total Emp Wage : 240
-//UC6 Day 1 Emp hr : 4
-//Day 2 Emp hr : 4
-//Day 3 Emp hr : 8
-//Day 4 Emp hr : 0
-//Day 5 Emp hr : 8
-//Total Emp Wage :480
